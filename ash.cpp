0198009c2d4d34a5ba43ad11402fefb49f64cdcf
Ashly Colmenares <67175780+Ashvtc@users.noreply.github.com>
2020-06-20 14:25:15 -0400

    Update ash.cpp

--- ash.cpp
+++ ash.cpp
@@ -1,34 +1,36 @@
-/********** Menu Base de listas ****/
+/********** Menu Base de dos listas ****/
 #include <stdio.h>
 #include <stdlib.h>
 struct lista{
 	int valor;
-	struct lista *prox;};
-	
-void insertar( struct lista **Ini, int x){
-/* inserta x por el comienzo de la lista p */
-	lista *t = new struct lista;
+	lista *prox;};
+
+void insertar( lista **Ini, int x){
+	/* inserta x por el comienzo de la lista p */
+	lista *t = new lista;
 	t->valor = x;
 	t->prox = *Ini;
 	*Ini = t;
 }
+
 void mostrar ( lista *Ini){
-/* muestra por pantalla la lista enlazada */
-	struct lista *t = Ini;
+	/* muestra por pantalla la lista enlazada */
+	lista *t = Ini;
 	while ( t){
-	printf("[%i]->", t->valor);
-	t= t->prox;};
+		printf("[%i]->", t->valor);
+		t= t->prox;};
 	printf("NULL\n\n");
 }
+
 void mostrarlst( lista *p, char c){
-/* imprime encabezado y llama a mostrar */
+	/* imprime encabezado y llama a mostrar */
 	printf( "\n\n\tLista %c ->",c);
 	mostrar( p);
 }
+
 int main() {
 	int op = -1, x=0, n;
 	lista *A=NULL,*B=NULL;
-	
 	while ( op ){
 		system("cls");
 		printf( "\n\n\t\tMENU DE MANEJO DE LISTAS \n\n\t ");
@@ -38,21 +40,20 @@ int main() {
 		printf( "0.\tSalir\n\n ");
 		printf( "\t\tSu opcion (0..3): ");
 		scanf("%i", &op);
-		
 		switch (op){
-			case 1: printf("\n\n\tIndique dato a Insertar en A ");
-					scanf( "%i", &x);
-					insertar (&A, x);
-				break;
-			case 2: printf("\n\n\tIndique dato a Insertar en B ");
-					scanf( "%i", &x);
-					insertar (&B, x);
-				break;
-			case 3: mostrarlst(A,'A');
-					mostrarlst(B,'B');
-				break;
+		case 1: printf("\n\n\tIndique dato a Insertar en A ");
+			scanf( "%i", &x);
+			insertar (&A, x);
+		break;
+		case 2: printf("\n\n\tIndique dato a Insertar en B ");
+			scanf( "%i", &x);
+			insertar (&B, x);
+		break;
+		case 3: mostrarlst(A,'A');
+			mostrarlst(B,'B');
+		break;
 		};
-		printf("\n\n");
-		system("pause");
-}
+	printf("\n\n");
+	system("pause");
+	}
 }/* FIN de Menu Base de dos Listas */
